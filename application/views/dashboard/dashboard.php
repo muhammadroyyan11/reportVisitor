75884f160f18b8ed3ea2156c751333b51df33f5d
Muhammad Royyan Zamzami <royyanmz87@gmail.com>
2023-02-26 16:28:13 +0700

    per day

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -148,18 +148,20 @@ if (userdata('role') == 1) { ?>
 
     <section class="content">
         <div class="row">
-            <div class="col-xs-12">
-                <div class="box">
-                    <div class="box-header">
-                        <h3 class="box-title">Data <?= $wisata->nama ?></h3>
+            <?php if ($wisata != null) { ?>
+                <div class="col-xs-12">
+                    <div class="box">
+                        <div class="box-header">
+                            <h3 class="box-title">Data <?= $wisata->nama ?></h3>
+                        </div>
+                        <div class="col-8">
+                            <!-- <canvas id="graph"></canvas> -->
+                            <div id="graph"></div>
+                        </div>
+                        <!-- /.box-body -->
                     </div>
-                    <div class="col-8">
-                        <!-- <canvas id="graph"></canvas> -->
-                        <div id="graph"></div>
-                    </div>
-                    <!-- /.box-body -->
                 </div>
-            </div>
+            <?php } ?>
         </div>
     </section>
 
